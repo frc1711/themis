9b5975a35d3132d970bdad200578ba84565267e7
Abigail Fox <abigailfireflyfox@gmail.com>
2018-02-10 18:22:44 -0500

    added timed intake

--- src/org/usfirst/frc/team1711/robot/commands/auton/TimedIntake.java
+++ src/org/usfirst/frc/team1711/robot/commands/auton/TimedIntake.java
@@ -25,6 +25,7 @@ public class TimedIntake extends TimedCommand {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() 
     {
+    	Robot.intake.intake();
     }
 
     // Called once after timeout
