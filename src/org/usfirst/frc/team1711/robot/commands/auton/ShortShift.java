b61d20e57dfb597cb040aef9a9e8d5a4c43525e2
Abigail Fox <abigailfireflyfox@gmail.com>
2018-02-28 21:53:01 -0500

    added camera (usb, raw smart dash stream)

---
+++ src/org/usfirst/frc/team1711/robot/commands/auton/ShortShift.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1711.robot.commands.auton;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * @author Lou
+ */
+public class ShortShift extends CommandGroup {
+
+    public ShortShift() {
+    	
+    	addParallel(new TimedLift(10, .5));
+    	addParallel(new AutoDrive(10));
+    	addSequential(new Turn(10));
+    	addSequential(new AutoDrive(10));
+    	addSequential(new TimedExpel(10));
+    }
+}
